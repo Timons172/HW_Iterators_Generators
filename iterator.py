058b16c728f875548eb19143db57943645930768
Тимур Ахмедов <timons@MacBook-Pro-Timur.local>
2024-10-24 18:49:35 +0200

    Initial commit

---
+++ iterator.py
@@ -0,0 +1,38 @@
+class FlatIterator:
+
+    def __init__(self, list_of_list):
+        self.new_list = [element for each_list in list_of_list for element in each_list]
+        self.item = []
+
+    def __iter__(self):
+        self.cursor = -1
+        return self
+
+    def __next__(self):
+        self.cursor += 1
+        if self.cursor == len(self.new_list):
+            raise StopIteration
+        self.item = self.new_list[self.cursor]
+        return self.item
+
+
+def test_1():
+
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+
+if __name__ == '__main__':
+    test_1()
